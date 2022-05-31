06dc4d98b201e28e99abbc8120c90b410eb5f624
mrcollier91 <colliermatt91@gmail.com>
2022-05-31 12:23:57 -0500

    commit tree dd3e895d40d432ee9043702de6c1ae260b36f002

---
+++ src/00-objectball.js
@@ -0,0 +1,62 @@
+function gameObject() {
+   return{
+     "home" : {
+        "teamName": "Brooklyn Nets",
+        "colors": ["Black", "White"],
+        "players":{
+            "Alan Anderson":{
+                "number": 0,
+                "shoe": 16,
+                "points": 22,
+                "Rebounds":12,
+                "assists": 12,
+                "steals": 3,
+                "blocks": 1,
+                "slamDunks": 1,
+            },
+            "Reggie Evans":{
+                "number": 30,
+                "shoe": 14,
+                "points": 12,
+                "Rebounds":12,
+                "assists": 12,
+                "steals": 12,
+                "blocks": 12,
+                "slamDunks": 7,
+            },
+            "Brook Lopez":{
+                "number": 11,
+                "shoe": 17,
+                "points": 17,
+                "Rebounds":19,
+                "assists": 10,
+                "steals": 3,
+                "blocks": 1,
+                "slamDunks": 15,
+            },
+            "Mason Plumlee":{
+                "number": 1,
+                "shoe": 19,
+                "points": 26,
+                "Rebounds":12,
+                "assists": 6,
+                "steals": 3,
+                "blocks": 8,
+                "slamDunks": 5,
+            },
+            "Jason Terry":{
+                "number": 31,
+                "shoe": 15,
+                "points": 19,
+                "Rebounds":2,
+                "assists": 2,
+                "steals": 4,
+                "blocks": 11,
+                "slamDunks": 1,
+            },
+
+     },
+    "away": {
+        
+    }
+}
